cd2947c8f8980783dd32799860ce33cb9ecd303f
10abe9 <90349540+10abe9@users.noreply.github.com>
2022-05-30 17:57:02 +0300

    added test_db
    checks whether the res are in database

---
+++ tests/test_db.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+def test_database():
+    conn = psycopg2.connect(dbname='productDb', user='admin', password='admin1234', host='localhost')
+    cursor = conn.cursor()
+    cursor.execute("select * from facew limit 1")
+    assert conn != None
+    assert cursor != None
+    assert cursor.fetchall() != []
+    

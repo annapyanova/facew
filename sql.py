0940dc4eeeba110d5223d0dbe4de52611f79597b
Anna <89931596+annapyanova@users.noreply.github.com>
2022-05-31 13:19:57 +0300

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,21 +1,18 @@
 import psycopg2
 
-conn = psycopg2.connect(dbname='productDb', user='admin', password='admin1234', host='localhost')
+conn = psycopg2.connect(dbname='productDb', user='admin', password='admin1234', host='postgresql_database')
 cursor = conn.cursor()
 
-#create_table_query = """
-#CREATE TABLE facew (
-#        o_id integer,
-#        class text,
-#        bbox_coords_xmin integer,
-#        bbox_coords_ymin integer,
-#        bbox_coords_xmax integer,
-#        bbox_coords_ymax integer
-#); 
-#"""
-#cursor.execute(create_table_query)
-#conn.commit()
-#print("РўР°Р±Р»РёС†Р° СѓСЃРїРµС€РЅРѕ СЃРѕР·РґР°РЅР° РІ PostgreSQL")
-
-cursor.execute("SELECT * FROM facew")
-print(cursor.fetchall())
+create_table_query = """
+CREATE TABLE facew (
+        o_id integer,
+        class text,
+        bbox_coords_xmin integer,
+        bbox_coords_ymin integer,
+        bbox_coords_xmax integer,
+        bbox_coords_ymax integer
+); 
+"""
+cursor.execute(create_table_query)
+conn.commit()
+print("Таблица успешно создана в PostgreSQL")

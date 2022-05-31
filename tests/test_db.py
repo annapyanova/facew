e8a8e2b47cac026339b8f1a8210c711e93e865aa
Anna <89931596+annapyanova@users.noreply.github.com>
2022-05-31 16:31:42 +0300

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 def test_database():
-    conn = psycopg2.connect(dbname='productDb', user='admin', password='admin1234', host='localhost')
+    conn = psycopg2.connect(dbname='productDb', user='admin', password='admin1234', host='postgresql_database')
     cursor = conn.cursor()
     cursor.execute("select * from facew limit 1")
     assert conn != None
